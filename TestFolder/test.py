71e4dd88a39275b05966d70f221bc0d53e7eb080
Dmitrii Ponomarev <ec22805@qmul.ac.uk>
2023-10-17 14:16:23 +0100

    Work

---
+++ TestFolder/test.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+sys.path.insert(0, "../ProjectFolder")
+from code import foo
+class TestFoo(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(foo(), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
