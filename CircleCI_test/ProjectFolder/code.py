2ef431ba2e59500a8867a6358bd02bf3dfbb2abb
Dmitrii Ponomarev <Dima11102003@gmail.com>
2023-11-17 10:48:51 +0000

    imports

--- CircleCI_test/ProjectFolder/code.py
+++ CircleCI_test/ProjectFolder/code.py
@@ -1,2 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
 def foo():
     return 1
--- CircleCI_test/TestFolder/test.py
+++ CircleCI_test/TestFolder/test.py
@@ -4,9 +4,13 @@ import sys
 sys.path.insert(0, "CircleCI_test/ProjectFolder")
 from code import foo
 
+
 class TestFoo(unittest.TestCase):
     def test1(self):
         self.assertEqual(foo(), 1)
+
+    def test2(self):
+        ...
 
 
 if __name__ == "__main__":
