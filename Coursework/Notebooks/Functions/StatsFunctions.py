656380f6fb2793299ee5970adc58572e5f7b2b3c
Dmitrii Ponomarev <Dima11102003@gmail.com>
2023-12-02 01:24:51 +0000

    More testing

--- Coursework/Notebooks/Functions/StatsFunctions.py
+++ Coursework/Notebooks/Functions/StatsFunctions.py
@@ -1,49 +1,47 @@
-class MyFunctions:
-    def pmcc(x: list, y: list):
-        if len(x) != len(y):
-            raise ValueError("Input arrays must have the same length")
+def pmcc(x: list, y: list):
+    if len(x) != len(y):
+        raise ValueError("Input arrays must have the same length")
 
-        # Calculate the correlation coefficient
-        num_points = len(x)
+    # Calculate the correlation coefficient
+    num_points = len(x)
 
-        # Calculate means
-        mean_x = sum(x) / num_points
-        mean_y = sum(y) / num_points
+    # Calculate means
+    mean_x = sum(x) / num_points
+    mean_y = sum(y) / num_points
 
-        # Calculate numerator and denominators
-        numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(num_points))
-        denominator_x = sum((x[i] - mean_x) ** 2 for i in range(num_points)) ** 0.5
-        denominator_y = sum((y[i] - mean_y) ** 2 for i in range(num_points)) ** 0.5
+    # Calculate numerator and denominators
+    numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(num_points))
+    denominator_x = sum((x[i] - mean_x) ** 2 for i in range(num_points)) ** 0.5
+    denominator_y = sum((y[i] - mean_y) ** 2 for i in range(num_points)) ** 0.5
 
-        # Calculate correlation coefficient
-        correlation_coefficient = numerator / (denominator_x * denominator_y)
-        return correlation_coefficient
+    # Calculate correlation coefficient
+    correlation_coefficient = numerator / (denominator_x * denominator_y)
+    return correlation_coefficient
 
-    def linear_regression(x: list, y: list):
-        if len(x) != len(y):
-            raise ValueError("Input arrays must have the same length")
 
-        # Calculate the means of x and y
-        mean_x = sum(x) / len(x)
-        mean_y = sum(y) / len(y)
+def linear_regression(x: list, y: list):
+    if len(x) != len(y):
+        raise ValueError("Input arrays must have the same length")
 
-        # Calculate the slope (m) and intercept (b) using least squares method
-        numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(len(x)))
-        denominator = sum((x[i] - mean_x) ** 2 for i in range(len(x)))
+    # Calculate the means of x and y
+    mean_x = sum(x) / len(x)
+    mean_y = sum(y) / len(y)
 
-        # Avoid division by zero
-        if denominator == 0:
-            raise ValueError(
-                "The denominator in the slope calculation is zero. Cannot perform linear regression."
-            )
+    # Calculate the slope (m) and intercept (b) using least squares method
+    numerator = sum((x[i] - mean_x) * (y[i] - mean_y) for i in range(len(x)))
+    denominator = sum((x[i] - mean_x) ** 2 for i in range(len(x)))
 
-        slope = numerator / denominator
-        intercept = mean_y - slope * mean_x
+    # Avoid division by zero
+    if denominator == 0:
+        raise ValueError(
+            "The denominator in the slope calculation is zero. Cannot perform linear regression."
+        )
 
-        return slope, intercept
+    slope = numerator / denominator
+    intercept = mean_y - slope * mean_x
 
-    def mean(x: list):
-        return round(sum(x) / len(x), 4)
+    return slope, intercept, pmcc(x, y)
 
-    def SD(x: list):
-        ...
+
+def mean(x: list):
+    return round(sum(x) / len(x), 5)
